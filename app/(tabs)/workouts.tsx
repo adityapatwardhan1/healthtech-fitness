4d0dcb7aafd2faaa0c2e9ec6bb1c0410d03bf1b3
Aditya Patwardhan <adityap.thecool@gmail.com>
2025-04-05 11:45:20 -0500

    Pushing workouts page

--- app/(tabs)/workouts.tsx
+++ app/(tabs)/workouts.tsx
@@ -1,16 +1,33 @@
 import { Text, View, StyleSheet } from 'react-native';
 import Button from '@/components/Button';
 
-export default function ShopScreen() {
+export default function WorkoutsScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Workouts</Text>
+      <Text style={styles.text}>My templates</Text>
+      <View style={styles.content}>
+      
+      <Button label="My templates"/>
+      <View style={{ flexDirection: 'row', justifyContent: 'flex-end', width: '100%' }}>
+        <Button label="+" style={{ width: 40, height: 40, justifyContent: 'center', alignItems: 'center' }}/>
+      </View>
+      <Button label="Back and Biceps"/>
+      <Button label="Glute-focused legs"/>
+      <Button label="Quad-focused legs"/>
+      <Button label="Push day"/>
+      <Button label="Pull day"/>
+      </View>
       <View style={styles.footerContainer}>
-        <Button label="My workouts" />
+        {/* <Button label="My workouts" /> */}
+        
       </View>
     </View>
   );
 }
+
+// Need screen for specific workout
+
 
 const styles = StyleSheet.create({
   container: {
@@ -28,8 +45,8 @@ const styles = StyleSheet.create({
     marginTop: 50,
   },
   content: {
-    flex: 1,
-    justifyContent: 'center',
+    flex: 0.5,
+    justifyContent: 'space-between',
     alignItems: 'center',
   },
   footerContainer: {
@@ -40,3 +57,60 @@ const styles = StyleSheet.create({
     color: '#fff',
   },
 });
+
+// import { Text, View, StyleSheet } from 'react-native';
+// import Button from '@/components/Button';
+
+// export default function ShopScreen() {
+//   return (
+//     <View style={styles.container}>
+//       <Text style={styles.title}>Workouts</Text>
+//       <Text style={styles.text}>My templates</Text>
+//       <View style={styles.content}>
+      
+//       <Button label="My templates"/>
+//       <View style={{ flexDirection: 'row', alignItems: 'right', width: '100%' }}>
+//         <Button label="+" style = {{width: '50%'}}/>
+//       </View>
+//       <Button label="Back and Biceps"/>
+//       <Button label="Glute-focused legs"/>
+//       <Button label="Quad-focused legs"/>
+//       <Button label="Push day"/>
+//       <Button label="Pull day"/>
+//       </View>
+//       <View style={styles.footerContainer}>
+//         {/* <Button label="My workouts" /> */}
+        
+//       </View>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#25292e',
+//   },
+//   header: {
+//     alignItems: 'center',
+//   },
+//   title: {
+//     color: '#fff',
+//     textAlign: 'center',
+//     fontSize: 32,
+//     fontWeight: 'bold',
+//     marginTop: 50,
+//   },
+//   content: {
+//     flex: 0.5,
+//     justifyContent: 'space-between',
+//     alignItems: 'center',
+//   },
+//   footerContainer: {
+//     padding: 2,
+//     alignItems: 'center',
+//   },
+//   text: {
+//     color: '#fff',
+//   },
+// });
